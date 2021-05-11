d5a626a8146737bb916ac1922f52d57f888d179c
khoaiz ahmmed <khoaizahmedctg@gmail.com>
2021-05-12 02:28:30 +0600

    database connect

--- config/database.js
+++ config/database.js
@@ -4,6 +4,7 @@ require('dotenv').config()
 const DBconnect = mysql.createPool({
     host:process.env.DB_HOST,
     user:process.env.DB_USER,
+    password:process.env.DB_PASS,
     database:process.env.DATABASE
   });
   module.exports = DBconnect
